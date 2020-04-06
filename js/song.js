ba52baa0ef5ac0a6805f14bfd999ff595b7f5679
sdhuchri <57616257+sdhuchri@users.noreply.github.com>
2020-04-06 21:19:29 +0700

    Add files via upload

---
+++ js/song.js
@@ -0,0 +1,10 @@
+var source = "refrain.mp3"
+var audio = document.createElement("audio");
+//
+audio.autoplay = true;
+//
+audio.load()
+audio.addEventListener("load", function() { 
+    audio.play(); 
+}, true);
+audio.src = source;
